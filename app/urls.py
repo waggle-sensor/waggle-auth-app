4fc5e398aabd9ec56afbbda320593916966971d1
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-09-14 17:13:27 -0500

    clean up and add app_name

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "app"
 
 urlpatterns = [
-    # path("profile-allow-list/<str:username>", views.allow_list),
-    path("<str:username>/access", views.profile_access),
-    # path("node-allow-list/<str:username>/develop", views.list_develop_access),
+    path("<str:username>/access", views.profile_access, name="profile_access"),
 ]
-
-# maybe we just have nodes belong to a single project. that
