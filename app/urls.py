ff109865c2433d2828d52eb8b9321beb0c3e301a
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-09-21 13:32:10 -0500

    added support for format suffix in urls

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from .views import ProfileAccessView
 
 app_name = "app"
 
-urlpatterns = [
+urlpatterns = []
+
+# add rest_framework endpoints
+urlpatterns += format_suffix_patterns([
     path("profiles/<str:username>/access", ProfileAccessView.as_view(), name="profile_access"),
-]
+])
