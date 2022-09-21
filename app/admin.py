db8daf255497ff7a3db36caa82a1f0a39a1a04c8
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-09-21 14:36:39 -0500

    order admin items by usernames and vsns

--- app/admin.py
+++ app/admin.py
@@ -3,15 +3,32 @@ from .models import Profile, Node, Project, ProfileMembership, NodeMembership
 
 
 class ProfileMembershipInline(admin.TabularInline):
+    ordering = ("profile__user__username",)
     model = ProfileMembership
     extra = 0
 
 
 class NodeMembershipInline(admin.TabularInline):
+    ordering = ("node__vsn",)
     model = NodeMembership
     extra = 0
 
 
-admin.site.register(Profile, inlines=(ProfileMembershipInline,))
-admin.site.register(Node, inlines=(NodeMembershipInline,))
-admin.site.register(Project, inlines=(ProfileMembershipInline, NodeMembershipInline), list_display=("name", "number_of_members", "number_of_nodes"))
+class ProfileAdmin(admin.ModelAdmin):
+    ordering = ("user__username",)
+    inlines = (ProfileMembershipInline,)
+
+
+class NodeAdmin(admin.ModelAdmin):
+    ordering = ("vsn",)
+    inlines = (NodeMembershipInline,)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = (ProfileMembershipInline, NodeMembershipInline)
+    list_display = ("name", "number_of_members", "number_of_nodes")
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Node, NodeAdmin)
+admin.site.register(Project, ProjectAdmin)
