265c6f0189d428c27ab47fa9fae68e4ff274f781
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-12-12 09:25:46 -0600

    reworked manifest serialization

--- manifests/views.py
+++ manifests/views.py
@@ -5,14 +5,23 @@ from .models import *
 from .serializers import NodeSerializer
 
 
+# BEFORE DOING ANY OF THIS, UPDATE THE TESTS TO INCLUDE ALL RELEVANT FIELDS!!!!
+
 class NodeList(ListCreateAPIView):
-    queryset = NodeData.objects.all().order_by("vsn")
+    # queryset = NodeData.objects.all()
+    queryset = NodeData.objects.all().prefetch_related(
+        "compute_set__hardware__capabilities",
+        "nodesensor_set__hardware",
+        "nodesensor_set__labels",
+        "resource_set__hardware__capabilities",
+        "tags",
+    )
     serializer_class = NodeSerializer
     permission_classes = [AllowAny]
 
 
 class NodeFilterList(RetrieveAPIView):
-    queryset = NodeData.objects.all().order_by("vsn")
+    queryset = NodeData.objects.all()
     serializer_class = NodeSerializer
     lookup_field = "vsn"
     permission_classes = [AllowAny]
