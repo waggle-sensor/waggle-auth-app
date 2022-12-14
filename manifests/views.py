73b89f99e10efa910bd9d4c2ae81b5ad21f14595
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-12-14 14:36:13 -0600

    using read only views for manifests APIs

--- manifests/views.py
+++ manifests/views.py
@@ -1,18 +1,18 @@
 from django.contrib.auth.models import *
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from .models import *
 from .serializers import NodeSerializer
 
 
-class NodeList(ListCreateAPIView):
+class NodeList(ListAPIView):
     queryset = NodeData.objects.all().order_by("vsn")
     serializer_class = NodeSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class NodeFilterList(RetrieveAPIView):
     queryset = NodeData.objects.all().order_by("vsn")
     serializer_class = NodeSerializer
     lookup_field = "vsn"
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
