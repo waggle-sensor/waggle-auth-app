0f2ead489afbb4adcc7fd979535e65262c47013a
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-12-15 11:59:19 -0600

    adding sensors apis and tests

--- manifests/urls.py
+++ manifests/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('manifests/', views.NodeList.as_view(), name="nodes"),
     path('manifests/<str:vsn>/', views.NodeFilterList.as_view(), name="node-filter"),
+    path('sensors/', views.SensorHardwareListView.as_view(), name="sensor_list"),
+    path('sensors/<str:hardware>/', views.SensorHardwareDetailView.as_view(), name="sensor_detail"),
 ]
--- manifests/views.py
+++ manifests/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import *
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import *
-from .serializers import NodeSerializer
+from .serializers import ManifestSerializer, SensorHardwareSerializer
 
 
 class NodeList(ListAPIView):
@@ -20,12 +20,38 @@ class NodeList(ListAPIView):
 
         "tags",
     ).order_by("vsn")
-    serializer_class = NodeSerializer
+    serializer_class = ManifestSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class NodeFilterList(RetrieveAPIView):
-    queryset = NodeData.objects.all().order_by("vsn")
-    serializer_class = NodeSerializer
+    queryset = NodeData.objects.all().prefetch_related(
+        "compute_set__hardware__capabilities",
+
+        "nodesensor_set__hardware__capabilities",
+        "nodesensor_set__labels",
+
+        "compute_set__computesensor_set__scope",
+        "compute_set__computesensor_set__hardware__capabilities",
+        "compute_set__computesensor_set__labels",
+
+        "resource_set__hardware__capabilities",
+
+        "tags",
+    )
+    serializer_class = ManifestSerializer
     lookup_field = "vsn"
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class SensorHardwareListView(ListAPIView):
+    queryset = SensorHardware.objects.all()
+    serializer_class = SensorHardwareSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class SensorHardwareDetailView(RetrieveAPIView):
+    queryset = SensorHardware.objects.all()
+    serializer_class = SensorHardwareSerializer
+    lookup_field = "hardware"
     permission_classes = [IsAuthenticatedOrReadOnly]
