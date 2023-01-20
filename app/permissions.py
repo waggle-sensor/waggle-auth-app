63a94c5a025066c3a03571bfc25ad3e52fa60fe2
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-01-20 14:03:37 -0600

    allow users to view their own access permissions

--- app/permissions.py
+++ app/permissions.py
@@ -2,5 +2,15 @@ from rest_framework import permissions
 
 
 class IsSelf(permissions.BasePermission):
+
     def has_object_permission(self, request, view, obj):
         return bool(request.user and (request.user == obj))
+
+
+class IsMatchingUsername(permissions.BasePermission):
+    # TODO Figure out how to do this in general. This is a hack to apply permissions
+    # to the user access view based on username in the URL.
+
+    def has_permission(self, request, view):
+        username = view.kwargs.get("username")
+        return bool(request.user and (request.user.username == username))
