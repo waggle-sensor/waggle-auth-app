f32ae52d20b4f1df0201e169d4980acb044a692c
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-04-10 09:04:44 -0500

    moved cors middleware before session middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,8 +26,8 @@ AUTH_USER_MODEL = "app.User"
 MIDDLEWARE = [
     "django_prometheus.middleware.PrometheusBeforeMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
