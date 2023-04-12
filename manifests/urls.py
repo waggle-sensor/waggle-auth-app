f00c9244728cf78e829f143ae3e0accc93d12558
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-04-12 10:38:25 -0500

    updated internal url names to make monitoring more clear

--- manifests/urls.py
+++ manifests/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path
 from . import views
 
+app_name = "manifests"
+
 urlpatterns = [
-    path("manifests/", views.NodeList.as_view(), name="nodes"),
-    path("manifests/<str:vsn>/", views.NodeFilterList.as_view(), name="node-filter"),
-    path("sensors/", views.SensorHardwareListView.as_view(), name="sensor_list"),
+    path(
+        "manifests/",
+        views.NodeList.as_view(),
+        name="manifest-list",
+    ),
+    path(
+        "manifests/<str:vsn>/",
+        views.NodeFilterList.as_view(),
+        name="manifest-detail",
+    ),
+    path(
+        "sensors/",
+        views.SensorHardwareListView.as_view(),
+        name="sensor-list",
+    ),
     path(
         "sensors/<str:hardware>/",
         views.SensorHardwareDetailView.as_view(),
-        name="sensor_detail",
+        name="sensor-detail",
     ),
 ]
