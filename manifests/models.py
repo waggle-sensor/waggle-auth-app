673b38d34e55a9ec9e3e937cd2f0134cdd5a22d0
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-10-06 15:41:10 -0500

    added more flags for lorawan device columns

--- manifests/models.py
+++ manifests/models.py
@@ -319,9 +319,9 @@ class NodeBuild(models.Model):
             )
             
 class LoRaWANDevice(models.Model):
-    node = models.ForeignKey(NodeData, on_delete=models.CASCADE, related_name='lorawandevices', blank=False)
-    DevEUI = models.UUIDField(unique=True)
-    device_name = models.TextField()
+    node = models.ForeignKey(NodeData, on_delete=models.CASCADE, related_name='lorawandevices', null=False, blank=False)
+    DevEUI = models.UUIDField(editable=False, unique=True, null=False, blank=False)
+    device_name = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_seen_at = models.DateTimeField(null=True, blank=True)
     battery_level = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
