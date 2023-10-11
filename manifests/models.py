d06d59d06bcc56f9b943c44ebe9be25273c02ea0
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-10-11 15:47:23 -0500

    change textfield to charfield

--- manifests/models.py
+++ manifests/models.py
@@ -321,7 +321,7 @@ class NodeBuild(models.Model):
 class LoRaWANDevice(models.Model):
     node = models.ForeignKey(NodeData, on_delete=models.CASCADE, related_name='lorawandevices', null=False,blank=False)
     DevEUI = models.CharField(max_length=16, primary_key=True, unique=True, null=False, blank=False)
-    device_name = models.TextField(null=True, blank=True)
+    device_name = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_seen_at = models.DateTimeField(null=True, blank=True)
     battery_level = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
