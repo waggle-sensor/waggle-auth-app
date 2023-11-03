3fbf1019198a8813307e4bd5786c17f9fc15f5fa
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-11-03 09:15:00 -0500

    customized token auth for users to use node model

--- node_auth/authentication.py
+++ node_auth/authentication.py
@@ -1,5 +1,100 @@
-from rest_framework import authentication
+"""
+Provides token authentication policies for nodes. - Grabbed from authentication module in rest_framework
+from rest_framework import authentication <- original
+"""
+import base64
+import binascii
+
+from django.contrib.auth import authenticate
+from django.middleware.csrf import CsrfViewMiddleware
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import HTTP_HEADER_ENCODING, exceptions
+from .models import Token
+
+def get_authorization_header(request):
+    """
+    Return request's 'Authorization:' header, as a bytestring.
+
+    Hide some test client ickyness where the header can be unicode.
+    """
+    auth = request.META.get('HTTP_AUTHORIZATION', b'')
+    if isinstance(auth, str):
+        # Work around django test client oddness
+        auth = auth.encode(HTTP_HEADER_ENCODING)
+    return auth
 
 
-class TokenAuthentication(authentication.TokenAuthentication):
-    keyword = "node_auth"
+class CSRFCheck(CsrfViewMiddleware):
+    def _reject(self, request, reason):
+        # Return the failure reason instead of an HttpResponse
+        return reason
+
+
+class BaseAuthentication:
+    """
+    All authentication classes should extend BaseAuthentication.
+    """
+
+    def authenticate(self, request):
+        """
+        Authenticate the request and return a two-tuple of (user, token).
+        """
+        raise NotImplementedError(".authenticate() must be overridden.")
+
+    def authenticate_header(self, request):
+        """
+        Return a string to be used as the value of the `WWW-Authenticate`
+        header in a `401 Unauthenticated` response, or `None` if the
+        authentication scheme should return `403 Permission Denied` responses.
+        """
+        pass
+
+class TokenAuthentication(BaseAuthentication):
+    """
+    Simple token based authentication.
+
+    Clients should authenticate by passing the token key in the "Authorization"
+    HTTP header, prepended with the string "node_auth ".  For example:
+
+        Authorization: node_auth 401f7ac837da42b97f613d789819ff93537bee6a
+    """
+
+    keyword = 'node_auth'
+    model = Token
+
+    def authenticate(self, request):
+        auth = get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != self.keyword.lower().encode():
+            return None
+
+        if len(auth) == 1:
+            msg = _('Invalid token header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _('Invalid token header. Token string should not contain spaces.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            msg = _('Invalid token header. Token string should not contain invalid characters.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        return self.authenticate_credentials(token)
+
+    def authenticate_credentials(self, key):
+        model = self.model
+        try:
+            token = model.objects.select_related('node').get(key=key)
+        except model.DoesNotExist:
+            raise exceptions.AuthenticationFailed(_('Invalid token.'))
+
+        # if not token.node.is_active: #need to add "is_active" to node model in manifest app
+        #     raise exceptions.AuthenticationFailed(_('User inactive or deleted.'))
+
+        return (token.node, token)
+
+    def authenticate_header(self, request):
+        return self.keyword
