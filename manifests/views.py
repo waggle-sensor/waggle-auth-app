0f0dc419583aa60489355e40fe9776f19921c6cf
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-11-08 12:54:57 -0600

    added node authentication for lorawanconnections retrieve api

--- manifests/views.py
+++ manifests/views.py
@@ -21,37 +21,7 @@ from rest_framework import status
 
 from node_auth.authentication import TokenAuthentication as NodeTokenAuthentication
 from node_auth.permissions import IsAuthenticated as NodeIsAuthenticated, IsAuthenticated_ObjectLevel as NodeIsAuthenticated_ObjectLevel
-
-# class NodeOwnedObjectsMixin:
-#     """
-#     Allows access to ONLY objects associated to authenticated node. Order of operations:
-#     1) token authentication
-#     2) filter queryset to only include records associated with node
-#     3) checks object permission
-#     """
-#     authentication_classes = (NodeTokenAuthentication,)
-#     permission_classes = (NodeIsAuthenticated_ObjectLevel,)
-#     def get_queryset(self):
-#         nodeVSN = self.request.user.vsn
-#         queryset = super().get_queryset()
-#         return queryset.filter(vsn=nodeVSN)
-
-class NodeOwnedObjectsMixin:
-    """
-    Allows access to ONLY objects associated with the authenticated node. Order of operations:
-    1) Token authentication
-    2) Filter queryset to only include records associated with the node (reference your model's vsn field)
-    3) Check object permission
-    """
-    authentication_classes = (NodeTokenAuthentication,)
-    permission_classes = (NodeIsAuthenticated_ObjectLevel,)
-    vsn_field = 'vsn'  # Default vsn field name
-
-    def get_queryset(self):
-        nodeVSN = self.request.user.vsn
-        queryset = super().get_queryset()
-        return queryset.filter(**{f'{self.vsn_field}': nodeVSN})
-
+from django.shortcuts import get_object_or_404
 
 class NodeAuthMixin:
     """
@@ -59,6 +29,38 @@ class NodeAuthMixin:
     """
     authentication_classes = (NodeTokenAuthentication,)
     permission_classes = (NodeIsAuthenticated,)
+
+class NodeOwnedObjectsMixin(NodeAuthMixin):
+    """
+    Allows access to ONLY objects associated with the authenticated node. Order of operations:
+    1) Token authentication
+    2) Filter queryset to only include records associated with the node (reference your model's vsn field)
+    3) Check object permission (custom views, you'll need to make sure you check the object level permission checks yourself)
+        - self.check_object_permissions(request, node_obj)
+    """
+    permission_classes = (NodeIsAuthenticated_ObjectLevel,)
+    vsn_field = 'vsn'  # Default vsn field name
+    node_field_name = 'node' # default node foreign key
+
+    def get_queryset(self):
+        nodeVSN = self.request.user.vsn
+        queryset = super().get_queryset()
+        return queryset.filter(**{f'{self.vsn_field}': nodeVSN})
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        # Assuming you are using a generic view, you can retrieve the object as follows
+        obj = get_object_or_404(queryset, **{self.lookup_field: self.kwargs[lookup_url_kwarg]})
+
+        # Check object-level permissions here using the user-specified node field name
+        if hasattr(obj,'vsn'):
+            self.check_object_permissions(self.request, obj)
+        else:
+            self.check_object_permissions(self.request, getattr(obj, self.node_field_name))
+
+        return obj
 
 class ManifestViewSet(ReadOnlyModelViewSet):
     queryset = (
@@ -151,15 +153,13 @@ class LorawanDeviceView(NodeAuthMixin, CreateAPIView, UpdateAPIView, RetrieveAPI
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class LorawanConnectionView(CreateAPIView, UpdateAPIView, RetrieveAPIView):
+class LorawanConnectionView(NodeOwnedObjectsMixin,CreateAPIView, UpdateAPIView, RetrieveAPIView):
     serializer_class = LorawanConnectionSerializer
-    queryset = LorawanConnection.objects.all()
-    permission_classes = [
-        IsAdminUser
-    ]  # adding this for now until node authentication architecture is created
+    vsn_field = "node__vsn" #reference relationship table's field (NodeData vsn field)
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
+        self.check_object_permissions(request, instance.node)
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
