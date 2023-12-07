720b7af578e312d5f381d753db891c7a31128b96
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-12-06 18:11:52 -0600

    re structured middleware code

---
+++ node_auth/contrib/auth/__init__.py
@@ -0,0 +1,52 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import HTTP_HEADER_ENCODING
+from node_auth import get_token_keyword, get_token_model
+from .models import AnonymousNode
+
+def get_node(request):
+    """
+    Get node instance using token in auth header. 
+    If no node is retrieved, return an instance of `AnonymousNode`.
+    """
+    KEYWORD = get_token_keyword()
+
+    auth = get_authorization_header(request).split()
+
+    if not auth or auth[0].lower() != KEYWORD.lower().encode():
+        return AnonymousNode()
+
+    if len(auth) == 1:
+        return AnonymousNode()
+    elif len(auth) > 2:
+        return AnonymousNode()
+
+    try:
+        token = auth[1].decode()
+    except UnicodeError:
+        return AnonymousNode()
+
+    return authenticate_credentials(token)
+
+def authenticate_credentials(key):
+    TOKEN_MODEL = get_token_model()
+    try:
+        token = TOKEN_MODEL.objects.select_related("node").get(key=key)
+    except TOKEN_MODEL.DoesNotExist:
+        return AnonymousNode()
+
+    if not token.node.is_active: 
+        return AnonymousNode()
+
+    return token.node
+
+def get_authorization_header(request):
+    """
+    Return request's 'Authorization:' header, as a bytestring.
+
+    Hide some test client ickyness where the header can be unicode.
+    """
+    auth = request.META.get("HTTP_AUTHORIZATION", b"")
+    if isinstance(auth, str):
+        # Work around django test client oddness
+        auth = auth.encode(HTTP_HEADER_ENCODING)
+    return auth
--- node_auth/contrib/auth/middleware.py
+++ node_auth/contrib/auth/middleware.py
@@ -1,66 +1,16 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.functional import SimpleLazyObject
-from django.utils.translation import gettext_lazy as _
-from rest_framework import HTTP_HEADER_ENCODING
-from node_auth import get_token_keyword, get_token_model, get_node_model
+from node_auth.contrib import auth
+
+def get_node(request):
+    return auth.get_node(request)
 
 class AuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request):
         """add the lazy node instance to request"""
-        self.node_model = get_node_model()
-        self.token_model = get_token_model()
-        self.keyword = get_token_keyword()
 
         # Retrieve Node instance
-        node_instance = SimpleLazyObject(lambda: self.get_node_instance(request))
+        node_instance = SimpleLazyObject(lambda: get_node(request))
 
         # Attach the 'node' attribute to the request
         request.node = node_instance
- 
-    def get_node_instance(self, request):
-        """
-        Get node instance using token in auth header. 
-        If no node is retrieved, return an instance of `AnonymousNode`.
-        """
-        from .models import AnonymousNode
-
-        auth = self.get_authorization_header(request).split()
-
-        if not auth or auth[0].lower() != self.keyword.lower().encode():
-            return AnonymousNode()
-
-        if len(auth) == 1:
-            return AnonymousNode()
-        elif len(auth) > 2:
-            return AnonymousNode()
-
-        try:
-            token = auth[1].decode()
-        except UnicodeError:
-            return AnonymousNode()
-
-        return self.authenticate_credentials(token)
-
-    def authenticate_credentials(self, key):
-        try:
-            token = self.token_model.objects.select_related("node").get(key=key)
-        except self.token_model.DoesNotExist:
-            return AnonymousNode()
-
-        if not token.node.is_active: 
-            return AnonymousNode()
-
-        return token.node
-
-    @staticmethod
-    def get_authorization_header(request):
-        """
-        Return request's 'Authorization:' header, as a bytestring.
-
-        Hide some test client ickyness where the header can be unicode.
-        """
-        auth = request.META.get("HTTP_AUTHORIZATION", b"")
-        if isinstance(auth, str):
-            # Work around django test client oddness
-            auth = auth.encode(HTTP_HEADER_ENCODING)
-        return auth
