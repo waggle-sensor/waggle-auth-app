a1d0eecc8feb62645eb3e6fd225c964f27e07f74
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-12-06 23:47:10 -0600

    added test for get functions

--- node_auth/__init__.py
+++ node_auth/__init__.py
@@ -1,6 +1,7 @@
 import django
 from django.apps import apps as django_apps
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 if django.VERSION < (3, 2):
     default_app_config = "rest_framework.authtoken.apps.AuthTokenConfig"
--- node_auth/tests/test_models.py
+++ node_auth/tests/test_models.py
@@ -3,8 +3,10 @@ from node_auth.models import Token
 import unittest
 from unittest.mock import patch
 from node_auth.models import Token
+from node_auth import get_token_keyword, get_node_model, get_token_model
+from django.core.exceptions import ImproperlyConfigured
 
-class test_TokenModel(unittest.TestCase):
+class test_TokenModel(TestCase):
 
     @patch("node_auth.models.os.urandom")
     def test_generate_key(self, mock_urandom):
@@ -25,3 +27,51 @@ class test_TokenModel(unittest.TestCase):
         result = str(token_instance)
 
         self.assertEqual(result, "test_key")
+
+class get_functions(TestCase):
+
+    @patch('node_auth.settings.AUTH_NODE_MODEL', 'invalid_model_format')
+    def test_get_node_model_ValueError(self):
+        """
+        test get_node_model raises ImproperlyConfigured for value error
+        """
+        with self.assertRaises(ImproperlyConfigured) as context:
+            get_node_model()
+
+    @patch('node_auth.settings.AUTH_NODE_MODEL', 'nonexistent_app.NonexistentModel')
+    def test_get_node_model_lookup_error(self):
+        """
+        test get_node_model raises ImproperlyConfigured for lookup error
+        """
+        with self.assertRaises(ImproperlyConfigured) as context:
+            get_node_model()
+
+    @patch('node_auth.settings.AUTH_NODE_TOKEN_MODEL', 'invalid_model_format')
+    def test_get_token_model_ValueError(self):
+        """
+        test get_token_model raises ImproperlyConfigured for value error
+        """
+        with self.assertRaises(ImproperlyConfigured) as context:
+            get_token_model()
+
+    @patch('node_auth.settings.AUTH_NODE_TOKEN_MODEL', 'nonexistent_app.NonexistentModel')
+    def test_get_token_model_lookup_error(self):
+        """
+        test get_token_model raises ImproperlyConfigured for lookup error
+        """
+        with self.assertRaises(ImproperlyConfigured) as context:
+            get_token_model()
+
+    @patch('node_auth.settings', None)
+    def test_get_token_keyword_attribute_error(self):
+        """
+        test get_token_keyword raises ImproperlyConfigured for attribute error
+        """
+        with self.assertRaises(ImproperlyConfigured) as context:
+            get_token_keyword()
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
