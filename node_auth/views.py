9bcb27cfdee0e2cb98995f3ecc293cf3eb70f5ad
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-01-22 16:00:50 -0600

    add node token read only model view set

--- node_auth/views.py
+++ node_auth/views.py
@@ -1,25 +1,15 @@
-# from rest_framework import parsers, renderers
-# from .models import Token
-# from .serializers import AuthTokenSerializer
-# from rest_framework.permissions import IsAdminUser
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
+from rest_framework import parsers, renderers
+from .models import Token
+from .serializers import AuthTokenSerializer
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from app.authentication import TokenAuthentication as UserTokenAuthentication
 
-# class ObtainAuthToken(APIView):
-#     throttle_classes = ()
-#     permission_classes = IsAdminUser
-#     parser_classes = (
-#         parsers.FormParser,
-#         parsers.MultiPartParser,
-#         parsers.JSONParser,
-#     )
-#     renderer_classes = (renderers.JSONRenderer,)
-#     serializer_class = AuthTokenSerializer
-
-#     def post(self, request, *args, **kwargs):
-#         # serializer = self.get_serializer(data=request.data)
-#         serializer = self.serializer_class(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         node = serializer.validated_data["node"]
-#         token, created = Token.objects.get_or_create(node=node)
-#         return Response({"token": token.key})
+class TokenViewSet(ReadOnlyModelViewSet):
+    permission_classes = [IsAdminUser]
+    authentication_classes = [UserTokenAuthentication]
+    serializer_class = AuthTokenSerializer
+    queryset = Token.objects.all().order_by("node__vsn")
+    lookup_field = "node__vsn"
