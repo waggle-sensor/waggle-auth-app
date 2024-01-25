56b88dc6b7792c7ce09e21bae4971aa012075e37
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-01-25 16:38:37 -0600

    I removed catch as functions return better errors

--- manifests/schema.py
+++ manifests/schema.py
@@ -8,35 +8,35 @@ from django.db import models
 from graphene_django.types import DjangoObjectType
 from .models import *
 
-APP_NAME = "manifests"
-
 class Query(graphene.ObjectType):
     AnsibleInventory = graphene.Field( #TODO: implement behavior where groupby can be left out
-        graphene.JSONString, 
+        graphene.JSONString,
+        groupby_app=graphene.String(required=True),
         groupby_model=graphene.String(required=True), 
-        groupby_name_attr=graphene.String(required=True), 
+        groupby_name_attr=graphene.String(required=True),
+        host_app=graphene.String(required=True),
         host_model=graphene.String(required=True), 
         host_name_attr=graphene.String(required=True), 
         vars=graphene.List(graphene.String),
         description="Returns a JSON object that can be used to design Ansible inventory scripts for Dynamic Inventory\
+            \n- groupby_app: The app the groupby model is apart of\
             \n- groupby_model: The model to group the host by (must be related to host)\
             \n- groupby_name_attr: The attribute to return as names for the group\
+            \n- host_app: The app the host model is apart of\
             \n- host_model: The model used for hosts\
             \n- host_name_attr: The attribute to return as host names\
             \n- vars: The attributes to return as variables for groups and hosts"
     )
 
-    def resolve_AnsibleInventory(self, info, groupby_model, groupby_name_attr, host_model, host_name_attr, vars, **kwargs):
+    def resolve_AnsibleInventory(self, info, groupby_app, groupby_model, groupby_name_attr, host_app, host_model, host_name_attr, vars, **kwargs):
+
+        #try to get apps
+        apps.get_app_config(groupby_app)
+        apps.get_app_config(host_app)
 
         # Get the model class's dynamically
-        try:
-            parent_model = apps.get_model(app_label=APP_NAME, model_name=groupby_model)
-        except LookupError:
-            raise GraphQLError(f"Invalid model name provided for groupby_model: {groupby_model} does not exist")
-        try:
-            child_model = apps.get_model(app_label=APP_NAME, model_name=host_model)
-        except LookupError:
-            raise GraphQLError(f"Invalid model name provided for host_model: {host_model} does not exist")
+        parent_model = apps.get_model(app_label=groupby_app, model_name=groupby_model)
+        child_model = apps.get_model(app_label=host_app, model_name=host_model)
 
         # Validate if ChildModel has a ForeignKey for ParentModel and get foreign key name
         foreign_key_name = None
@@ -48,19 +48,18 @@ class Query(graphene.ObjectType):
             raise GraphQLError(f"{host_model} does not have a foreign key for {groupby_model}")
 
         # Validate if the name_attr(s) is a valid field in models
-        parent_fields = [field.name for field in parent_model._meta.fields]
-        if groupby_name_attr not in parent_fields:
-            raise GraphQLError(f"{groupby_name_attr} does not exist in {groupby_model}")
+        parent_model.objects.values_list(groupby_name_attr, flat=True)
+        child_model.objects.values_list(host_name_attr, flat=True)
 
+        #split fields
+        parent_fields = [field.name for field in parent_model._meta.fields]
         child_fields = [field.name for field in child_model._meta.fields]
-        if host_name_attr not in child_fields:
-            raise GraphQLError(f"{host_name_attr} does not exist in {host_model}")
 
         # Validate vars exist in parent_model or child_model
         all_fields = set(parent_fields) | set(child_fields)
         missing_fields = [x for x in vars if x not in all_fields]
         if missing_fields:
-            raise GraphQLError(f"{', '.join(missing_fields)} do not exist in {groupby_model} and {host_model}")
+            raise GraphQLError(f"{', '.join(missing_fields)} field(s) do not exist in {groupby_model} and {host_model}")
 
         # split attr by model
         parent_vars = []
