9e808fe0a5926d7c80b0a8eca8e1c8419686fa2a
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-01-25 15:00:48 -0600

    removed graphene behavior of auto camel casing names

--- config/schema.py
+++ config/schema.py
@@ -16,4 +16,4 @@ class Query(manifests.schema.Query):
 
 
 # schema = graphene.Schema(query=Query, mutation=Mutation)
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query,auto_camelcase=False)
