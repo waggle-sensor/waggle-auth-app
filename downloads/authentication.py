68da9ad6a90bcfe4405ec4c4f48b1c9c36135b09
Sean Shahkarami <sean.shahkarami@gmail.com>
2024-01-31 16:52:47 -0600

    updated doc

--- downloads/authentication.py
+++ downloads/authentication.py
@@ -9,6 +9,17 @@ from rest_framework import exceptions
 # This authenticators MUST only be used for the downloads view and NEVER as a default authenticator. Tokens are
 # intended to be scoped to specific views and not intended to grant a full user session. In the case a token is
 # leaked, that would essentially allow someone to hijack and entire user's account.
+#
+# Some context on why this is here:
+#
+# This was added because the way wget forwards Authentication headers is incompatible with S3. Specifically,
+# after getting the presigned URL, wget forwards the header to S3, unlike curl or Python's requests which
+# drop it for security purposes. Once S3 receives this header, it returns 400 Bad Request...
+#
+# By sheer luck, the way wget handles HTTP Basic Auth is to hit the endpoint and only sends auth if if a
+# WWW-Authenticate header is returned. This means, the presigned URL works!
+#
+# It also happens to be compatible to examples we showed users in our docs before, which is a nice side effect.
 class BasicTokenPasswordAuthentication(BasicAuthentication):
     def authenticate_credentials(self, userid, password, request=None):
         user, auth = TokenAuthentication().authenticate_credentials(password)
