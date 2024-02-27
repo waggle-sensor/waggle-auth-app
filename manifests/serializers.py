d10e0cf7e4b962379e43cf66dbd8214b3e696d50
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-02-27 16:39:06 -0600

    added normalized address fields

--- manifests/serializers.py
+++ manifests/serializers.py
@@ -372,7 +372,15 @@ class NodesSerializer(serializers.ModelSerializer):
     project = SlugRelatedField(read_only=True, slug_field='name')
     focus = SlugRelatedField(read_only=True, slug_field='name')
     partner = SlugRelatedField(read_only=True, slug_field='name')
-    address = SlugRelatedField(read_only=True, slug_field='formatted')
+    address_formatted = serializers.CharField(source='address.formatted', read_only=True)
+    address_streetnum = serializers.CharField(source='address.street_number', read_only=True)
+    address_route = serializers.CharField(source='address.route', read_only=True)
+    address_town = serializers.CharField(source='address.locality.name', read_only=True)
+    address_postal_code = serializers.CharField(source='address.locality.postal_code', read_only=True)
+    address_state = serializers.CharField(source='address.locality.state.name', read_only=True)
+    address_state_code = serializers.CharField(source='address.locality.state.code', read_only=True)
+    address_country = serializers.CharField(source='address.locality.state.country.name', read_only=True)
+    address_country_code = serializers.CharField(source='address.locality.state.country.code', read_only=True)
 
     class Meta:
         model = NodeData
@@ -387,7 +395,16 @@ class NodesSerializer(serializers.ModelSerializer):
                   "gps_lat",
                   "gps_lon",
                   "gps_alt",
-                  "address",
+                  "address_formatted",
+                  "address_streetnum",
+                  "address_route",
+                  "address_town",
+                  "address_postal_code",
+                  "address_state",
+                  "address_state_code",
+                  "address_state_code",
+                  "address_country",
+                  "address_country_code",
                   "location", 
                   "phase",
                   "commissioned_at",
