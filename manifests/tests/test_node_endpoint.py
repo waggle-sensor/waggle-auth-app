bc4cb9b799c4a8cf5d6fcd842fa4f985981c2f0a
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-03-06 16:21:15 -0600

    added more test and changed fx to use helper fxs

--- manifests/tests/test_node_endpoint.py
+++ manifests/tests/test_node_endpoint.py
@@ -2,13 +2,11 @@ from django.test import TestCase
 from manifests.models import *
 from address.models import *
 from pytest import mark
+from ManifestHelp_fx import *
 
-#TODO: add a test for url filtering 
-
-@mark.rn
 class NodesViewSetTestCase(TestCase):
     def setUp(self):
-        self.createComputeHardware(
+        createComputeHardware(
             [
                 {
                     "hardware": "xaviernx", 
@@ -32,7 +30,7 @@ class NodesViewSetTestCase(TestCase):
             ]
         )
 
-        self.createSensorHardware(
+        createSensorHardware(
             [
                 {
                     "hardware": "bme280", 
@@ -65,42 +63,42 @@ class NodesViewSetTestCase(TestCase):
             ]
         )
 
-        self.createLorawanDevice(
+        createLorawanDevice(
             [
                 {"deveui":"123","name":"CSU_soil_sensor","hardware":"Arduino MKR WAN 1310"},
                 {"deveui":"111","name":"test","hardware":"lorawan_2"},
             ]
         )
 
-        self.createProject(
+        createProject(
             [
                 {"name": "Sage"},
                 {"name": "MyProject"}
             ]
         )
 
-        self.createFocus(
+        createFocus(
             [
                 {"name": "URBAN"},
                 {"name": "MyFocus"}
             ]
         )
 
-        self.createPartner(
+        createPartner(
             [
                 {"name": "NU"},
                 {"name": "MyPartner"}
             ]
         )
 
-        self.createSite(
+        createSite(
             [
                 {"id": "ANL","description": "this site is argonne national lab"},
                 {"id": "TEST","description": "this site is test"}
             ]
         )
 
-        self.createAddress(
+        createAddress(
             [
                 {   
                     "formatted": "9700 S Cass Ave, Lemont, IL 60439, USA",
@@ -138,7 +136,7 @@ class NodesViewSetTestCase(TestCase):
             ]
         )
 
-        self.createModem(
+        createModem(
             [
                 {
                     "sim_type": "anl-dawn",
@@ -159,10 +157,8 @@ class NodesViewSetTestCase(TestCase):
             ]
         )
 
-        self.createManifests(
-            [
-                {
-                    "id": 717,
+        self.W123_data = {
+                    "id": 1,
                     "vsn": "W123",
                     "name": "000048B02D0766BE",
                     "project": "Sage",
@@ -183,7 +179,7 @@ class NodesViewSetTestCase(TestCase):
                     "country": "United States",
                     "country_code": "US",
                     "location": "other notes on location",
-                    "phase": "Deployed",
+                    "phase": "Maintenance",
                     "commissioned_at": "2023-02-23T23:47:35Z",
                     "registered_at": "2023-02-23T23:47:35Z",
                     "modem_sim": "anl-dawn",
@@ -245,9 +241,10 @@ class NodesViewSetTestCase(TestCase):
                             ]
                         }
                     ],
-                },
-                {
-                    "id": 701,
+                }
+
+        self.W021_data = {
+                    "id": 2,
                     "vsn": "W021",
                     "name": "000048B02D15BC77",
                     "project": "MyProject",
@@ -313,172 +310,64 @@ class NodesViewSetTestCase(TestCase):
                             ]
                         }
                     ]
-                },
-            ]
-        )
+                }
 
-    def createComputeHardware(self, hardware):
-        """
-        Helper function which populates compute hardware test data.
-        """
-        for item in hardware:
-            compute_hardware = ComputeHardware.objects.create(
-                hardware=item["hardware"], 
-                hw_model=item["hw_model"],
-                manufacturer=item['manufacturer']
-            )
-
-        if "capabilities" in item:
-            for name in item["capabilities"]:
-                capability, _ = Capability.objects.get_or_create(capability=name)
-                compute_hardware.capabilities.add(capability)
-
-    def createSensorHardware(self, hardware):
-        """
-        Helper function which populates sensor hardware test data.
-        """
-        for item in hardware:
-            sensor_hardware = SensorHardware.objects.create(
-                hardware=item["hardware"], 
-                hw_model=item["hw_model"],
-                manufacturer=item['manufacturer']
-            )
-
-        if "capabilities" in item:
-            for name in item["capabilities"]:
-                capability, _ = Capability.objects.get_or_create(capability=name)
-                sensor_hardware.capabilities.add(capability)
-
-    def createLorawanDevice(self, device):
-        """
-        Helper function which populates lorawan device test data.
-        """
-        for item in device:
-            LorawanDevice.objects.create(
-                deveui=item["deveui"], name=item["name"], 
-                hardware=SensorHardware.objects.get(hardware=item["hardware"])
-            )
-
-    def createProject(self, project):
-        """
-        Helper function which populates project test data.
-        """
-        for item in project:
-            NodeBuildProject.objects.create(
-                name=item['name']
-            )
-
-    def createFocus(self, focus):
-        """
-        Helper function which populates focus test data.
-        """
-        for item in focus:
-            NodeBuildProjectFocus.objects.create(
-                name=item['name']
-            )
-
-    def createPartner(self, partner):
-        """
-        Helper function which populates partner test data.
-        """
-        for item in partner:
-            NodeBuildProjectPartner.objects.create(
-                name=item['name']
-            )
-
-    def createSite(self, site):
-        """
-        Helper function which populates site test data.
-        """
-        for item in site:
-            Site.objects.create(
-                id=item['id'], description=item['description']
-            )
-
-    def createAddress(self, address):
-        """
-        Helper function which populates address test data.
-        """
-        for item in address:
-            Country.objects.get_or_create(
-                name= item['country'], code=item['country_code']
-            )
-
-            State.objects.get_or_create(
-                name= item['state'], code=item['state_code'],
-                country=Country.objects.get(name=item['country'])
-            )
-
-            Locality.objects.get_or_create(
-                name= item['town'], postal_code=item['postal_code'],
-                state=State.objects.get(name=item['state'])
-            )
-
-            Address.objects.get_or_create(
-                street_number=item['streetnum'],route=item['route'], formatted=item['formatted'],
-                locality=Locality.objects.get(postal_code=item['postal_code'])
-            )
-
-    def createModem(self, modem):
-        """
-        Helper function which populates modem test data.
-        """        
-        for item in modem:
-            Modem.objects.create(
-                imei=item['imei'],imsi=item['imsi'],
-                iccid=item['iccid'],carrier=item['carrier'],
-                model=item['model'],sim_type=item['sim_type']
-            )
-
-    def createManifests(self, manifests):
-        """
-        Helper function which populates manifest test data.
-        """
-        for manifest in manifests:
-            node_obj = NodeData.objects.create(
-                vsn=manifest["vsn"],
-                name=manifest["name"],
-                gps_lat=manifest["gps_lat"],
-                gps_lon=manifest["gps_lon"],
-                gps_alt=manifest["gps_alt"],
-                project=NodeBuildProject.objects.get(name=manifest['project']),
-                focus=NodeBuildProjectFocus.objects.get(name=manifest['focus']),
-                partner=NodeBuildProjectPartner.objects.get(name=manifest['partner']),
-                type=manifest['type'],
-                site_id=Site.objects.get(id=manifest['site_id']),
-                address=Address.objects.get(formatted=manifest['addr_formatted']),
-                location=manifest["location"],
-                phase=manifest['phase'],
-                commissioned_at=manifest['commissioned_at'],
-                registered_at=manifest['registered_at']
-            )
-            #update modem
-            modem = Modem.objects.get(sim_type=manifest['modem_sim'])
-            modem.node = node_obj
-            modem.save()
-            for sensor in manifest['sensors']:
-                if "lorawan" in sensor['capabilities']:
-                    LorawanConnection.objects.create(
-                        node=node_obj,
-                        connection_type="OTAA", #in this view con_type does not matter so set a static val
-                        lorawan_device=LorawanDevice.objects.get(name=sensor['name'])
-                    )
-                else:
-                    NodeSensor.objects.create(
-                        node=node_obj,
-                        name=sensor['name'],
-                        hardware=SensorHardware.objects.get(hw_model=sensor['hw_model'])
-                    )
-            for compute in manifest["computes"]:
-                compute_obj = Compute.objects.create(
-                    node=node_obj,
-                    hardware=ComputeHardware.objects.get(hw_model=compute["hw_model"]),
-                    name=compute["name"],
-                )
+        createManifests([ self.W123_data, self.W021_data ])
 
     def test_retrieve_node(self):
-        """Test node view for retrieving a record happy path"""
-
-        r = self.client.get("/api/v-beta/nodes/W030")
+        """Test node view's happy path for retrieving a record """
+        r = self.client.get("/api/v-beta/nodes/W123/")
         self.assertEqual(r.status_code, 200)
-        self.assertEqual(len(r.json()), 1)
+        data = r.json()
+        self.assertTrue(isinstance(data, dict)) #returns dict
+        self.assertEqual(len(data), 29) #29 key-value pairs
+        self.assertDictEqual(data,self.W123_data) 
+
+    def test_retrieve_nodes(self):
+        """Test nodes view's happy path for retrieving multiple records"""
+        r = self.client.get("/api/v-beta/nodes/")
+        self.assertEqual(r.status_code, 200)
+        data = r.json()
+        self.assertTrue(isinstance(data, list))
+        self.assertEqual(len(data), 2) #2 nodes were created in setup()
+        self.assertEqual(data[0]['vsn'], self.W021_data['vsn']) #ordered by vsn so W021 is index 0
+
+    def test_project_url_filtering(self):
+        """Test nodes view's url filtering for project"""
+        r = self.client.get("/api/v-beta/nodes/?project__name=MyProject")
+        data = r.json()
+        self.assertTrue(isinstance(data, list))
+        self.assertEqual(len(data), 1) #one node in this project
+        self.assertEqual(data[0]['vsn'], self.W021_data['vsn'])
+
+    def test_project_url_mult_filtering(self):
+        """Test nodes view's url filtering for multiple projects"""
+        r = self.client.get("/api/v-beta/nodes/?project__name=MyProject,Sage")
+        data = r.json()
+        self.assertTrue(isinstance(data, list))
+        self.assertEqual(len(data), 2) #2 since one node in MyProject and other in Sage
+        self.assertEqual(data[1]['vsn'], self.W123_data['vsn']) #ordered by vsn so W123 is index 1
+
+    def test_phase_url_filtering(self):
+        """Test nodes view's url filtering for phase"""
+        r = self.client.get("/api/v-beta/nodes/?phase=Maintenance")
+        data = r.json()
+        self.assertTrue(isinstance(data, list))
+        self.assertEqual(len(data), 1) #one node in this phase
+        self.assertEqual(data[0]['vsn'], self.W123_data['vsn'])
+
+    def test_phase_url_mult_filtering(self):
+        """Test nodes view's url filtering for multiple phase"""
+        r = self.client.get("/api/v-beta/nodes/?phase=Maintenance,Deployed")
+        data = r.json()
+        self.assertTrue(isinstance(data, list))
+        self.assertEqual(len(data), 2) #2 since one node in Maint and other in Dep
+        self.assertEqual(data[0]['vsn'], self.W021_data['vsn']) #ordered by vsn so W021 is index 0
+
+    def test_mix_url_filtering(self):
+        """Test nodes view's url filtering for multiple filters"""
+        r = self.client.get("/api/v-beta/nodes/?project__name=MyProject,Sage&phase=Maintenance")
+        data = r.json()
+        self.assertTrue(isinstance(data, list))
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]['vsn'], self.W123_data['vsn'])
