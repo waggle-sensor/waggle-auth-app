5e2ba03757bbc39d302ce19d38e154c649ac361a
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-11-12 11:46:03 -0600

    added type return by func

--- opa/__init__.py
+++ opa/__init__.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-def get_opa_host():
+def get_opa_host() -> str:
     """
     Return the host for Open Policy Agent.
     """
@@ -12,7 +12,7 @@ def get_opa_host():
             "AUTHZ_OPA_HOST str needs to be configured in settings"
         )
 
-def get_opa_port():
+def get_opa_port()-> int:
     """
     Return the port used by Open Policy Agent.
     """
@@ -23,14 +23,14 @@ def get_opa_port():
             "AUTHZ_OPA_PORT int needs to be configured in settings"
         )
     
-def get_opa_default_policy():
+def get_opa_default_policy() -> str:
     """
     Return the default Open Policy Agent policy name.
     Uses "policy" as the default if AUTHZ_OPA_DEFAULT_POLICY is not set in settings.
     """
     return getattr(settings, "AUTHZ_OPA_DEFAULT_POLICY", "policy")
 
-def get_opa_default_rule():
+def get_opa_default_rule() -> str:
     """
     Return the default Open Policy Agent rule name.
     Uses "allow" as the default if AUTHZ_OPA_DEFAULT_RULE is not set in settings.
