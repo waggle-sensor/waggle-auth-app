15e3ee615825d1b7c25ddf0b0bfddd2b845f1cc7
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-04-01 11:08:20 -0500

    added initial loading script

---
+++ scripts/load_manifest.py
@@ -0,0 +1,6 @@
+"""
+This script is used to load the manifest data into the database. 
+Its purpose is to try to automize fields that are not dependent on user input.
+"""
+
+from manifests.models import *
